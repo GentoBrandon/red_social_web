5c786709b6353f36754719eed2ba21c233eff86e
kevinmartinez02 <kmartinezv7@miumg.edu.gt>
2024-10-28 16:41:51 -0600

    configuracion ORM 'knex' para conexion base de datos

---
+++ backend/src/migrations/20241028223337_create_table_persons.ts
@@ -0,0 +1,24 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  const exist = await knex.schema.hasTable('persons');
+  if(!exist){
+    await knex.schema.createTable('person',(table)=>{
+        table.increments('id').primary().unique().notNullable();
+        table.string('first_name').notNullable();
+        table.string('last_name').notNullable();
+        table.date('birth_date').notNullable();
+        table.string('email').unique().notNullable();
+    })
+  }
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    const exist = await knex.schema.hasTable('persons');
+    if(exist){
+        await knex.schema.dropTable('persons');
+    }
+}
+
