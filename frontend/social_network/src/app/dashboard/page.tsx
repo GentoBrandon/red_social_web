72364097cc482acddd8cfa115838723cd8a6c6b9
Brandon Gento <brandonailon19@gmail.com>
2024-10-29 15:17:47 -0600

    Integracion de barra lateral

--- frontend/social_network/src/app/dashboard/page.tsx
+++ frontend/social_network/src/app/dashboard/page.tsx
@@ -1,10 +1,10 @@
 import Principal from "@/component/Principal/Principal";
-
+import NavBar from "@/component/Nav/NavBar";
 function dashboardPage() {
   return (
-   <Principal>
-      <h1>Dashboard</h1>
-   </Principal>
+    <Principal>
+        <h1>Dashboard</h1>
+    </Principal>
   );
 }
 export default dashboardPage;
--- frontend/social_network/src/component/Principal/Principal.tsx
+++ frontend/social_network/src/component/Principal/Principal.tsx
@@ -1,14 +1,18 @@
-import NavBar from "../Nav/NavBar";
+'use client';
 import { ReactNode } from 'react';
+import { SidebarProvider} from "@/components/ui/sidebar"
+import { AppSidebar } from "../../component/Sidebar/Sidebar"
+import NavBar from '../Nav/NavBar';
 
 function Principal({ children }: { children: ReactNode }) {
     return (
-        <>
-        <NavBar/>
-        <main>
-            {children}
-        </main>
-        </>
+        <SidebarProvider>
+            <AppSidebar/>
+            <main>
+                <NavBar/>
+                {children}
+            </main>
+        </SidebarProvider> 
     )
 }
 export default Principal;
