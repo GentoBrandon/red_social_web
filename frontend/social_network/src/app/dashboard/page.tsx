ac269c121517e75bbf6b66df36a587d18e13a681
Brandon Gento <brandonailon19@gmail.com>
2024-10-30 16:54:21 -0600

    Integracion de estilos en principal

--- frontend/social_network/src/app/dashboard/page.tsx
+++ frontend/social_network/src/app/dashboard/page.tsx
@@ -1,17 +1,20 @@
 import Principal from "@/component/Principal/Principal";
 import Publication from "@/component/Publication/Publication";
 import TweetCard from "@/components/ui/tweet-card";
+import styles from "@/styles/Principal.module.css";
 function dashboardPage() {
   return (
     
     <Principal>
-      <Publication />
-      <div>
-      <TweetCard 
-        title="Mi primer día en el campus"
-        content="Hoy es mi primer día en la escuela @_buildspace! Un lugar donde conviertes tus ideas en realidad y haces amigos en el camino."
-      />
-    </div>
+      <div className={styles["mainContainer"]}>
+          <Publication />
+        <div className={styles["cardContainer"]}>
+          <TweetCard 
+          title="Mi primer día en el campus"
+          content="Hoy es mi primer día en la escuela @_buildspace! Un lugar donde conviertes tus ideas en realidad y haces amigos en el camino."
+          />
+        </div>
+      </div>
     </Principal>
   );
 }
