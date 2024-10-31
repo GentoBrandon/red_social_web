e6aeab45713b8484a09e2cf544bf9336700e3c4e
kevinmartinez02 <kmartinezv7@miumg.edu.gt>
2024-10-31 10:26:36 -0600

    creacion de ruta para login

--- backend/src/modules/auth/routes/authRoutes.ts
+++ backend/src/modules/auth/routes/authRoutes.ts
@@ -1,6 +1,7 @@
 import AuthController from "../controllers/authCotroller";
 import { Router } from "express";
 import UserSingUp from "../middleware/userSignUp";
+import auth from "../../../middleware/auth";
 const router = Router();
 
 router.post('/register',[
@@ -9,4 +10,5 @@ router.post('/register',[
     UserSingUp.hashPassword
     ],
     AuthController.createPersonWithUserCredentials);
+router.post('/login',auth.verifyToken,AuthController.login);
 export default router;
