e3b44426e3dd553861fa8302961f5fb91c7306fb
Brandon Gento <brandonailon19@gmail.com>
2024-11-01 11:24:25 -0600

    Integracion de estilos

--- frontend/social_network/src/components/ui/button.tsx
+++ frontend/social_network/src/components/ui/button.tsx
@@ -15,6 +15,8 @@ const buttonVariants = cva(
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+        btnGreen:
+          "bg-green-400 text-white shadow-sm hover:bg-green-500 hover:text-white",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
