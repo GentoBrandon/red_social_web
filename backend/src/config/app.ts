530b167e177df489b4f15ab246c4d3b02ce29380
kevinmartinez02 <kmartinezv7@miumg.edu.gt>
2024-11-01 10:52:23 -0600

    configuracion de cors y socket.io

--- backend/src/config/app.ts
+++ backend/src/config/app.ts
@@ -5,14 +5,23 @@ import userCredentialsRoutes from '../modules/user-credentials/routes/userCreden
 import profileRoutes from '../modules/profile/routes/profileRoutes';
 import authRoutes from '../modules/auth/routes/authRoutes';
 import cookieParser from 'cookie-parser';
+// configuracion de sockect.io
+import http from 'http';
+import {Server} from 'socket.io';
 import cors from 'cors'
 class App {
   private app: express.Application;
   private _PORT: number;
+  private _server : http.Server;
+  private _io : Server;
   public constructor(PORT: number) {
     this.app = express();
     this._PORT = PORT;
+    this._server = http.createServer(this.app);
+    this._io = new Server(this._server);
   }
+
+
   private settings(): void {
     this.app.use(cors({
       origin: 'http://localhost:3000',
@@ -31,15 +40,23 @@ class App {
     this.app.use(erroHandling);
   }
   private startServer(): void {
-    this.app.listen(this._PORT, () => {
+    this._server.listen(this._PORT, () => {
       console.log(`Server running on port ${this._PORT}`);
+    });
+  }
+  private settingsSocketIo(): void {
+    this._io.on('connection', (socket) => {
+      console.log('a user connected');
+      socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
     });
   }
   private startSettings(): void {
     this.settings();
     this.routes();
     this.middlewares();
-
+    this.settingsSocketIo();
     this.startServer();
   }
   public start() {
