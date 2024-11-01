5884b045a76d31412cfec79bd93cb79004b4bb1b
Brandon Gento <brandonailon19@gmail.com>
2024-10-31 22:58:22 -0600

    Integracion de nuevos campos

--- frontend/social_network/src/component/FormLogin/SignUp.tsx
+++ frontend/social_network/src/component/FormLogin/SignUp.tsx
@@ -1,19 +1,32 @@
 import styles from '../../styles/SignUpForm.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function SignUpForm() {
   return (
     <div className={styles.container}>
       <div className={styles.frame}>
         <form className={styles.formSignup}>
-          <label htmlFor="fullname">Full Name</label>
-          <input className={styles.formStyling} type="text" name="fullname" placeholder="Full Name" />
+        <div className="row">
+            <div className="col">
+              <label htmlFor="fullname">Nombres</label>
+              <input className={styles.formStyling} type="text" name="first_name" placeholder="Ingrese su nombre" required/>
+            </div>
+            <div className="col">
+              <label htmlFor="fullname">Apellidos</label>
+              <input className={styles.formStyling} type="text" name="last_name" placeholder="Ingrese su apellido" required/>
+            </div>
+          </div>
+          <label htmlFor="fullname">Nombre de usuario</label>
+          <input className={styles.formStyling} type="text" name="user_name" placeholder="Ingrese su nombre de usuario" required/>
+          <label htmlFor="fullname">Fecha de nacimiento</label>
+          <input className={styles.formStyling} type="date" name="birth_date" placeholder="Ingrese su fecha de nacimiento" required/>
           <label htmlFor="email">Email</label>
-          <input className={styles.formStyling} type="email" name="email" placeholder="Email" />
+          <input className={styles.formStyling} type="email" name="email" placeholder="Email" required/>
           <label htmlFor="password">Password</label>
-          <input className={styles.formStyling} type="password" name="password" placeholder="Password" />
+          <input className={styles.formStyling} type="password" name="password" placeholder="Password" required />
           <label htmlFor="confirmpassword">Confirm Password</label>
-          <input className={styles.formStyling} type="password" name="confirmpassword" placeholder="Confirm Password" />
-          <button type="button" className={styles.btnSignup}>Sign up</button>
+          <input className={styles.formStyling} type="password" name="confirmpassword" placeholder="Confirm Password" required/>
+          <button type="submit" className={styles.btnSignup}>Registrarse</button>
         </form>
       </div>
     </div>
