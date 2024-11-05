bc47f3c396fc2ddbb9ecfce1c20c6806a4f47185
kevinmartinez02 <kmartinezv7@miumg.edu.gt>
2024-11-04 23:16:06 -0600

    update: rutas para friends

--- backend/src/modules/profile/routes/requestFriendRoutes.ts
+++ backend/src/modules/profile/routes/requestFriendRoutes.ts
@@ -2,25 +2,18 @@ import RequestFriendController from '../controllers/requestFriendController';
 import { Router } from 'express';
 const router = Router();
 router.post('/create-request-friend', RequestFriendController.createRequest);
-router.get('/get-request-friend-all', RequestFriendController.getRequestAll);
-router.get(
-  '/get-request-friend-by-id/:id',
-  RequestFriendController.getRequestById,
-);
+
 router.put(
   '/accepted-friend/:id1/:id2',
   RequestFriendController.acceptFriendRequest,
 );
+
 router.delete(
-  '/delete-request-friend/:id',
-  RequestFriendController.deleteRequest,
-);
-router.delete(
-  '/delete-friend/:id1/:id2',
+  '/reject-friend/:id1/:id2',
   RequestFriendController.rejectFriendRequest,
 );
 router.get(
-  '/get-friends-by-name/:id',
+  '/get-friends-commons-friends/:id',
   RequestFriendController.getFriendsByProfileId,
 );
 export default router;
