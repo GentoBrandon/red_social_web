ab3e186c94d943a81b41145fe33b07b43cf1f6b3
Brandon Gento <brandonailon19@gmail.com>
2024-11-04 18:07:48 -0600

    Integracion de estilos y funcinalidades

--- frontend/social_network/src/component/Nav/NavBar.tsx
+++ frontend/social_network/src/component/Nav/NavBar.tsx
@@ -9,8 +9,9 @@ import {
     MenubarSeparator,
     MenubarTrigger,
   } from "@/components/ui/menubar";
+import style from '../../styles/Profile/NavBar.module.css'
 import Notifications from "@/component/Nav/Notifications";
-import { FC, useState } from "react";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import { API_ROUTES } from "@/routes/apiRoutes";
@@ -19,7 +20,7 @@ function NavBar() {
 
     const [profile, setProfile] = useState<null | any>(null);
     const router = useRouter();
-
+    // Función para cerrar sesión
     const handleProfile = async (): Promise<void> => {
       try {
         await axios.post(API_ROUTES.LOGOUT,{},{ withCredentials: true });
@@ -31,31 +32,39 @@ function NavBar() {
     };
 
     return (
-      <Menubar>
-        <MenubarMenu>
-          <MenubarTrigger>Inicio</MenubarTrigger>
-        </MenubarMenu>
-        <MenubarMenu>
-          <MenubarTrigger>Amigos</MenubarTrigger>
-        </MenubarMenu>
-        <MenubarMenu>
-          <MenubarTrigger>Notificaciones</MenubarTrigger>
-          <MenubarContent>
-            <Notifications />
-          </MenubarContent>
-        </MenubarMenu>
-        <MenubarMenu>
-          <MenubarTrigger onClick={() => router.push('/users/profile')}>Perfil</MenubarTrigger>
-          <MenubarContent>
-            <MenubarRadioGroup value="benoit">
-              <MenubarRadioItem value="#">Perfil</MenubarRadioItem>
-            </MenubarRadioGroup>
-            <MenubarSeparator />
-            <MenubarItem inset>Configuraciones</MenubarItem>
-            <MenubarSeparator />
-            <MenubarItem inset onClick={handleProfile}>Cerrar sesion</MenubarItem>
-          </MenubarContent>
-        </MenubarMenu>
+      <Menubar className={style.Menubar}>
+        <div className={style.MenubarMenu}>
+          <MenubarMenu>
+            <MenubarTrigger onClick={() => router.push('/dashboard')}>Inicio</MenubarTrigger>
+          </MenubarMenu>
+        </div>
+        <div className={style.MenubarMenu}>
+          <MenubarMenu>
+            <MenubarTrigger>Amigos</MenubarTrigger>
+          </MenubarMenu>
+        </div>
+        <div className={style.MenubarMenu}>
+          <MenubarMenu>
+            <MenubarTrigger>Notificaciones</MenubarTrigger>
+            <MenubarContent>
+              <Notifications />
+            </MenubarContent>
+          </MenubarMenu>
+        </div>
+        <div className={style.MenubarMenu}>
+          <MenubarMenu>
+            <MenubarTrigger onClick={() => router.push('/users/profile')}>Perfil</MenubarTrigger>
+            <MenubarContent>
+              <MenubarRadioGroup value="benoit">
+                <MenubarRadioItem value="#">Perfil</MenubarRadioItem>
+              </MenubarRadioGroup>
+              <MenubarSeparator />
+              <MenubarItem inset>Configuraciones</MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem inset onClick={handleProfile}>Cerrar sesion</MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+        </div>
       </Menubar>
     )
   }
--- frontend/social_network/src/components/ui/menubar.tsx
+++ frontend/social_network/src/components/ui/menubar.tsx
@@ -42,7 +42,7 @@ const MenubarTrigger = React.forwardRef<
   <MenubarPrimitive.Trigger
     ref={ref}
     className={cn(
-      "flex cursor-default select-none items-center rounded-sm px-3 py-1 text-sm font-medium outline-none focus:bg-accent focus:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground",
+      "flex cursor-default select-none items-center rounded-sm px-4 py-2 text-base font-semibold outline-none focus:bg-accent focus:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground", // Ajuste el tamaño de fuente a text-base y px/py
       className
     )}
     {...props}
