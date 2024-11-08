b1750ec002749cb04f255d53286025acefc6d586
Alej011 <agallardoc1@miumg.edu.gt>
2024-11-08 15:24:34 -0600

    ruta para buscar por nombre

--- backend/src/modules/profile/routes/requestFriendRoutes.ts
+++ backend/src/modules/profile/routes/requestFriendRoutes.ts
@@ -2,25 +2,25 @@ import RequestFriendController from '../controllers/requestFriendController';
 import { Router } from 'express';
 const router = Router();
 router.post('/create-request-friend', RequestFriendController.createRequest);
-
-router.put(
-  '/accepted-friend/:id1/:id2',
-  RequestFriendController.acceptFriendRequest,
+router.get('/get-request-friend-all', RequestFriendController.getRequestAll);
+router.get(
+  '/get-request-friend-by-id/:id',
+  RequestFriendController.getRequestById,
 );
 
 router.delete(
-  '/reject-friend/:id1/:id2',
-  RequestFriendController.rejectFriendRequest,
+  '/delete-request-friend/:id',
+  RequestFriendController.deleteRequest,
 );
+
 router.get(
   '/get-friends-commons-friends/:id',
   RequestFriendController.getFriendsByProfileId,
 );
 
-router.get(
-  '/get-status-friends/:id1/:id2',
-  RequestFriendController.getStatusFriend,
-);
 router.get('/get-requests-friend/:id', RequestFriendController.getRequestById);
 router.get('/get-received-friend/:id',RequestFriendController.getReceibedRequest)
+
+
+router.get('/friends/search/:id', RequestFriendController.searchFriendsByName);
 export default router;
