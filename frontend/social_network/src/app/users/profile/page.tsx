8f9743331aac65c45ad12e725f993726c8a34a59
Brandon Gento <brandonailon19@gmail.com>
2024-11-07 18:14:15 -0600

    Renderizar los posts compartidos

--- frontend/social_network/src/app/users/profile/page.tsx
+++ frontend/social_network/src/app/users/profile/page.tsx
@@ -1,11 +1,13 @@
 import Principal from '@/component/Principal/Principal';
 import Profile from '@/component/Profile/Profile';
 import Posts from '@/component/Profile/Posts';
+import PostsShare from '@/component/Posts/PostsShare';
 function ProfilePage(){
     return(
         <Principal>
             <Profile>
                 <Posts/>
+                <PostsShare/>
             </Profile>
         </Principal>
     )
